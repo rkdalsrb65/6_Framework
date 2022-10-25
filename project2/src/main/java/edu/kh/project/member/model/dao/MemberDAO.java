1e6d9d7dbee15923a7fa4e28f1e6199f792fef34
rkdalsrb65 <rkdalsrb65@naver.com>
2022-10-25 12:46:50 +0900

    MemberDAO

---
+++ project2/src/main/java/edu/kh/project/member/model/dao/MemberDAO.java
@@ -0,0 +1,19 @@
+package edu.kh.project.member.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+import edu.kh.project.member.model.vo.Member;
+
+@Repository // 퍼시스턴스 레이어, 영속성(파일, DB)을 가진 클래스 + bean 등록
+public class MemberDAO {
+
+	/** 로그인 DAO
+	 * @param memberEmail
+	 * @return loginMember
+	 */
+	public Member login(String memberEmail) {
+		
+		return loginMember;
+	}
+
+}
