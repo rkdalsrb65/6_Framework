7be2b6fd02bdb792b1bbf290eab30b6a0ebb129d
rkdalsrb65 <rkdalsrb65@naver.com>
2022-10-25 14:44:29 +0900

    MemberDAO

--- project2/src/main/java/edu/kh/project/member/model/dao/MemberDAO.java
+++ project2/src/main/java/edu/kh/project/member/model/dao/MemberDAO.java
@@ -1,5 +1,7 @@
 package edu.kh.project.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import edu.kh.project.member.model.vo.Member;
@@ -7,13 +9,17 @@ import edu.kh.project.member.model.vo.Member;
 @Repository // 퍼시스턴스 레이어, 영속성(파일, DB)을 가진 클래스 + bean 등록
 public class MemberDAO {
 
+	// DBCP + 마이바티스 이용 객체 DI(의존성 주입)
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 	/** 로그인 DAO
 	 * @param memberEmail
 	 * @return loginMember
 	 */
 	public Member login(String memberEmail) {
-		
-		return null;
+		//		 sqlSession.selectOne("매퍼이름.태그id", SQL 작성 시 필요한 값);
+		return sqlSession.selectOne("memberMapper.login", memberEmail);
 	}
 
 }
