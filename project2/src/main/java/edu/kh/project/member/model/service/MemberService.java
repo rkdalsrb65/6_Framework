d88c61560b001e7f5c2a1ddfaa8938aef960c83e
rkdalsrb65 <rkdalsrb65@naver.com>
2022-10-26 15:13:42 +0900

    MemberService

--- project2/src/main/java/edu/kh/project/member/model/service/MemberService.java
+++ project2/src/main/java/edu/kh/project/member/model/service/MemberService.java
@@ -17,4 +17,10 @@ public interface MemberService {
 	 * @return loginMember
 	 */
 	public abstract Member login(Member inputMember);
+
+	/** 회원 가입 서비스
+	 * @param inputMember
+	 * @return result
+	 */
+	public abstract int signUp(Member inputMember);
 }
