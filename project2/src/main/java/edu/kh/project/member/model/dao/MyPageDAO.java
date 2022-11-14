0fb77c491934baa204ad6331787f94a53df6939c
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-14 17:39:29 +0900

    MyPageDAO

--- project2/src/main/java/edu/kh/project/member/model/dao/MyPageDAO.java
+++ project2/src/main/java/edu/kh/project/member/model/dao/MyPageDAO.java
@@ -50,5 +50,13 @@ public class MyPageDAO {
 		return sqlSession.update("myPageMapper.memberDelete", memberNo);
 	}
 
+	/** 프로필 이미지 수정 DAO
+	 * @param loginMember
+	 * @return result
+	 */
+	public int updateProfile(Member loginMember) {
+		return sqlSession.update("myPageMapper.updateProfile", loginMember);
+	}
+
 	
 }
