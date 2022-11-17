a32db01f642424a2a181b6a14ddf5427e19f8bb2
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-18 08:54:28 +0900

    project2

---
+++ project2/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -0,0 +1,47 @@
+package edu.kh.project.board.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.kh.project.board.model.sevice.BoardService;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	private BoardService service;
+	
+	// 특정 게시판 목록 조회
+	// /board/1
+	// /board/2
+	// /board/3
+	// /board/4
+	// -> @PathVariable 사용
+	// URL 경로에 있는 값을 파라미터(변수)로 사용할 수 있게하는 어노테이션
+	// + 자동으로 request scope로 등록되어 EL 구문으로 jsp에서 출력도 가능
+	
+	// 요청주소?K=V&K=V&K=V... (queryString, 쿼리스트링)
+	// -> 요청주소에 값을 담아서 전달할 때 사용하는 문자열
+	
+	@GetMapping("/board/{boardCode}") // 주소에서 값을 꺼내서 변수에 저장한다.
+	public String selectBoardList(@PathVariable("boardCode") int boardCode,
+			Model model,
+			@RequestParam(value="cp", required = false, defaultValue="1") int cp // @RequestParam : 요청시 전달된 값을 받아오는 어노테이션
+			) {
+		// Model : 값 전달용 객체
+		// model.addAttribute("k", v) : request scope에 세팅
+		//									-> forward 시 유지됨
+		
+		Map<String, Object> map = service.selectBoardList(boardCode, cp);
+		
+		model.addAttribute("map", map); // request scope 세팅
+		
+		return "board/boardList"; // forward
+	}
+}
--- project2/src/main/webapp/WEB-INF/views/common/header.jsp
+++ project2/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -69,14 +69,16 @@
                 <li><a href="#">FAQ</a></li>
                 <li><a href="#">1:1 문의</a></li> --%>
 
-                <c:forEach var="boardType" items="${boardTypeMap}">
+                <c:forEach var="boardType" items="${boardTypeList}">
                     <%--
                         EL을 이용해서 Map 데이터를 다루는 방법
                         key   ==> ${변수명.key}
                         value ==> ${변수명.value}
                     --%>
                 
-                    <li><a href="/board/${boardType.key}/list">${boardType.value}</a></li>
+                    <li>
+                        <a href="/board/${boardType.BOARD_CODE}">${boardType.BOARD_NAME}</a>
+                    </li>
 
                 </c:forEach>
             </ul>
