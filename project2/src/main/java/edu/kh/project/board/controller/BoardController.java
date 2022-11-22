b31ac3f8e904a89c2919aa1059a8d77dd2d599b7
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-22 17:23:37 +0900

    BoardController

--- project2/src/main/java/edu/kh/project/board/controller/BoardController.java
+++ project2/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -1,25 +1,30 @@
 package edu.kh.project.board.controller;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.board.model.sevice.BoardService;
@@ -267,7 +272,49 @@ public class BoardController {
 		return "board/boardWrite";
 	}
 	
-	
+	// 게시글 작성
+	@PostMapping("/write/{boardCode}")
+	public String boardWrite(Board board,
+			@RequestParam(value="images", required=false) List<MultipartFile> imageList,
+			@SessionAttribute("loginMember")Member loginMember,
+			@PathVariable("boardCode") int boardCode,
+			RedirectAttributes ra, HttpSession session,
+			@RequestHeader("referer") String referer
+			) throws IOException {
+		
+		// 1. boardCode를 board객체에 세팅(Board에 boardCode 필드 추가)
+		board.setBoardCode(boardCode);
+		
+		// 2. 로그인한 회원의 번호를 board 객체 세팅
+		board.setMemberNo(loginMember.getMemberNo());
+		
+		// 3. 업로드된 파일의 웹 접근경로/서버 내부 경로 준비
+		String webPath = "/resources/images/board/";
+		
+		String folderPath = session.getServletContext().getRealPath(webPath);
+		// -> /resources/imaes/board/ 까지의 컴퓨터 저장 경로 반환
+
+		// 4. 게시글 삽입 서비스 호출
+		int boardNo = service.boardWrite(board, imageList, webPath, folderPath);
+		
+		String message = null;
+		String path = null;
+		
+		if(boardNo > 0) {
+						// /board/1/2003 (상세조회 요청 주소)
+			path = "/board/" + boardCode + "/" + boardNo;
+			message = "게시글이 등록되었습니다.";
+				
+		} else {
+			
+			path = referer;
+			message = "게시글 작성 실패";
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
+		return "redirect:" + path;
+	}
 
 	
 	
